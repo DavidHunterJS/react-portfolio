80fff9d032127b42204d92bf6dcc78a7c7b21d6a
David Hunter <david@davidhunter.io>
2021-04-21 18:27:05 -0700

    added resume file and link to it

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,7 +40,9 @@ export default function Home() {
           Solutions
         </motion.h1>
         <motion.a
-          href="#"
+          href="%PUBLIC_URL%/David_Hunter_Resume_04.21.21.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
           key="a"
           className="resume-cta"
           variants={resumeVariants}
