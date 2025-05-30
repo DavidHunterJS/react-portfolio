828e3c5d1635e725d3251e09d58796b97dc934c3
Lazer Beem <lazer@trippy.wtf>
2025-05-30 14:08:58 -0700

    created new project entry for fakebook and removed drudge reader

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,10 +1,19 @@
 import currencies from "./img/currency-converter.gif";
 import journeys from "./img/journeys.gif";
 import portfolio from "./img/portfolio.gif";
-import drudge from "./img/drudge.jpg";
+import fakebook from "./img/fakebook.gif";
 export const projectsData = [
   {
     key: 1,
+    image: fakebook,
+    title: "Fakebook - Full Stack Social Media Platform",
+    description:
+      "A modern, feature-rich Facebook clone built with cutting-edge JavaScript technologies and enterprise-grade DevOps practices.",
+    demo: "https://trippy.wtf",
+    code: "https://github.com/davidhunterjs/fakebook-frontend",
+  },
+  {
+    key: 2,
     image: currencies,
     title: "Currency Converter",
     description:
@@ -13,7 +22,7 @@ export const projectsData = [
     code: "https://github.com/davidhunterjs/currency-converter",
   },
   {
-    key: 2,
+    key: 3,
     image: journeys,
     title: "Journeys Website",
     description:
@@ -23,22 +32,13 @@ export const projectsData = [
   },
 
   {
-    key: 3,
+    key: 4,
     image: portfolio,
     title: "React Portfolio",
     description:
       "This website is deployed to AWS Amplify, created with React-Router, Framer-Motion and Bootstrap; using an AWS Lambda backend for the form email.",
     demo: "http://davidhunter.io",
     code: "https://github.comdavidhunterjs/react-portfolio",
-  },
-  {
-    key: 4,
-    image: drudge,
-    title: "Drudge Reader",
-    description:
-      "A full stack javascript app that scrapes the drudge report website and displays the content in a more pleasing manner with real-time updates. The app was made using Express, Mongoose and Redux",
-    demo: "https://trippy.wtf",
-    code: "https://github.comdavidhunterjs/drudge-reader-server",
   },
 ];
 export default projectsData;
